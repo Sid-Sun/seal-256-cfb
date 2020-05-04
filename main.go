e72be0377f9d46eecb74c3960f7635c3c64494ea
Sidharth Soni (Sid Sun) <sid@sidsun.com>
2020-05-05 02:06:00 +0530

    Comment code, Minor refactors, Remove deprecated code which had been commented, change flags (deprecate -flag), update help, add versioning, tweak functionality a lil
    Old Mc'donald had a farm... uh wait, what? Why am i typing that here? Guess we'll never know
    
    Signed-off-by: Sidharth Soni (Sid Sun) <sid@sidsun.com>

--- main.go
+++ main.go
@@ -13,41 +13,53 @@ import (
 	"github.com/sid-sun/sealion"
 )
 
+const version string = "1.0.0" // Program Version
+
 func main() {
 	var toEncrypt bool
 	var outputPath string
 	var err error
 
 	var sealionCipher cipher.Block
-	var passPhrase []byte
-
 	var inputStream, outputStream chan []byte
 	var progressStream chan int64
 	var wg sync.WaitGroup
 
 	if len(os.Args) == 4 || len(os.Args) == 5 {
-		if os.Args[1] == "-e" || os.Args[1] == "--encrypt" || os.Args[1] == "-encrypt" {
+		switch os.Args[1] {
+		case "-e", "--encrypt":
 			toEncrypt = true
-		} else if os.Args[1] == "-h" || os.Args[1] == "--help" || os.Args[1] == "-help" {
-			fmt.Printf("\nUsage:\n")
-			fmt.Printf("    For encryption: %s (--encrypt / -encrypt / -e) <input file> <passphrase file> <output file (optional)>.\n", os.Args[0])
-			fmt.Printf("    For decryption: %s (--decrypt / -decrypt / -d) <encrypted input> <passphrase file> <output file (optional)>.\n", os.Args[0])
-		} else if !(os.Args[1] == "-d" || os.Args[1] == "--decrypt" || os.Args[1] == "-decrypt") {
+		case "-h", "--help":
+			printHelp()
+		case "-d", "--decrypt":
+		default:
 			fmt.Println("Invalid argument:", os.Args[1])
+			os.Exit(1)
 		}
 
+		// If there's a 5th arguement, treat it as output file name
 		if len(os.Args) == 5 {
 			outputPath = os.Args[4]
 		}
 
-		passPhrase = readFromFile(os.Args[3])
-		key := sha256.Sum256(passPhrase)
+		// If the input file does not exist, print so and quit
+		if !fileExists(os.Args[2]) {
+			fmt.Println("File:", os.Args[2], "seems to be nonexistent")
+			os.Exit(1)
+		}
 
+		// Read contents of passphrase file and pass it through SHA-256
+		key := sha256.Sum256(readFromFile(os.Args[3]))
+
+		// Create the sealion cipher from the hash of passphrase
 		sealionCipher, err = sealion.NewCipher(key[:])
 		if err != nil {
 			panic(err.Error())
 		}
 
+		// Run 10 samples through standard sealion block encryption
+		// And measure the average time taken to encrypt a block
+		// So we can make a buffer of the appropriate size for the current machine
 		samples := 10
 		sampleBytes := make([]byte, sealion.BlockSize)
 		var avg time.Duration
@@ -58,89 +70,77 @@ func main() {
 			avg += time.Now().Sub(t0)
 		}
 
-		rate := int64(float64(sealion.BlockSize*samples) / float64(avg.Nanoseconds()) * 1000 * sealion.BlockSize * 1000)
-		inputStream = make(chan []byte, rate) // 65536 - 1048576 - 524288 - 540672 - 655360*
-		outputStream = make(chan []byte, rate)
-		progressStream = make(chan int64)
+		// Calculate the buffer size by finding the average number of bytes encrypted
+		// Each nano second (in float) and multiplying the result by the blocksize and 1000 * 1000
+		// The multiplication by these specific magic numbers is based on some logic which I came up with
+		// But failed to properly document - will be explained at some point in the unforseeable future
+		bufferSize := int64(float64(sealion.BlockSize*samples) / float64(avg.Nanoseconds()) * sealion.BlockSize * 1000000)
 
+		// Create buffere input, output and progress channels with the calculated buffer size
+		inputStream = make(chan []byte, bufferSize)
+		outputStream = make(chan []byte, bufferSize)
+		progressStream = make(chan int64, bufferSize)
+
+		// Start the reader
 		wg.Add(1)
-		go startReader(os.Args[2], &inputStream, &progressStream, &wg)
-
+		go readInput(os.Args[2], &inputStream, &progressStream, &wg)
 	} else {
-		fmt.Printf("Usage:\n")
-		fmt.Printf("    For encryption: %s (--encrypt / -encrypt / -e) <input file> <passphrase file> <output file (optional)>.\n", os.Args[0])
-		fmt.Printf("    For decryption: %s (--decrypt / -decrypt / -d) <encrypted input> <passphrase file> <output file (optional)>.\n", os.Args[0])
-		os.Exit(0)
+		// Check for version
+		if len(os.Args) > 1 {
+			if os.Args[1] == "-v" || os.Args[1] == "--version" {
+				fmt.Println(version)
+				os.Exit(0)
+			}
+		}
+		printHelp()
+		os.Exit(1)
 	}
 
+	// If the ouput file was not specified append .seal to the input file
+	// And use it as output file path
 	if outputPath == "" {
 		outputPath = os.Args[2] + ".seal"
 	}
 
-	// Run passphrase through SHA256 to get key
-	// key := sha256.Sum256(passPhrase)
-	// outputStream := make(chan []byte, )
-
+	// Start the encrypt / decrypt and output writer routines
 	wg.Add(2)
 	if toEncrypt {
-		// go encrypt(key[:], &inputStream, &outputStream, &wg)
 		go encrypt(&sealionCipher, &inputStream, &outputStream, &wg)
 	} else {
 		go decrypt(&sealionCipher, &inputStream, &outputStream, &wg)
 	}
-	go startWriter(outputPath, &outputStream, &wg)
+	go writeOutput(outputPath, &outputStream, &wg)
 
+	// The first output from progress stream is total file size
+	// Send that as target to progressbar function along with the progress stream
+	// For subsequent progress reporting
 	progressBar(<-progressStream, &progressStream)
+
+	// Wait until all goroutines have ended before quitting
 	wg.Wait()
 }
 
 func encrypt(sealionCipher *cipher.Block, inputStream, outputStream *chan []byte, wg *sync.WaitGroup) {
-	// sealionCipher, err := sealion.NewCipher(key)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
+	// Defer waitgroup go-routine done before returning
+	defer wg.Done()
 
+	// Generate a random initialisation vector for CFB
 	iv := make([]byte, (*sealionCipher).BlockSize())
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		panic(err.Error())
 	}
 
-	//copy(iv, "Welll, this good")
-
 	// Push IV to output stream
 	*outputStream <- iv
 
-	// Create new CFB Encryptor
+	// Create CFB Encrypter
 	cfb := cipher.NewCFBEncrypter(*sealionCipher, iv)
 
 	for {
+		// Fetch full / part block from inputStream
 		block := <-*inputStream
-		if block == nil {
-			// Push nil to output stream and break out of loop
-			*outputStream <- nil
-			break
-		}
 
-		// Run CFB on blockbyte
-		cfb.XORKeyStream(block, block)
-		// Push block to output stream
-		*outputStream <- block
-	}
-
-	wg.Done()
-}
-
-func decrypt(sealionCipher *cipher.Block, inputStream, outputStream *chan []byte, wg *sync.WaitGroup) {
-	// seaLionCipher, err := sealion.NewCipher(key)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-
-	iv := <-*inputStream
-	cfb := cipher.NewCFBDecrypter(*sealionCipher, iv)
-
-	for {
-		block := <-*inputStream
+		// A nil in block signals end of file
 		if block == nil {
 			// Push nil to output stream and break out of loop
 			*outputStream <- nil
@@ -152,6 +152,34 @@ func decrypt(sealionCipher *cipher.Block, inputStream, outputStream *chan []byte
 		// Push block to output stream
 		*outputStream <- block
 	}
+}
 
-	wg.Done()
+func decrypt(sealionCipher *cipher.Block, inputStream, outputStream *chan []byte, wg *sync.WaitGroup) {
+	// Defer waitgroup go-routine done before returning
+	defer wg.Done()
+
+	// First input from input stream is the IV
+	iv := <-*inputStream
+
+	// Create CFB decrypter with IV
+	// Potential error of IV not being of the same size as cipher Block Size
+	// Is handled in NewCFBDecrypter already
+	cfb := cipher.NewCFBDecrypter(*sealionCipher, iv)
+
+	for {
+		// Read block from input stream
+		block := <-*inputStream
+
+		// A nil block signals end of input
+		if block == nil {
+			// Push nil to output stream and break out of loop
+			*outputStream <- nil
+			break
+		}
+
+		// Run CFB on block
+		cfb.XORKeyStream(block, block)
+		// Push block to output stream
+		*outputStream <- block
+	}
 }
--- utils.go
+++ utils.go
@@ -10,104 +10,135 @@ import (
 	"github.com/sid-sun/sealion"
 )
 
-func startReader(fileName string, stream *chan []byte, progressStream *chan int64, wg *sync.WaitGroup) {
-	if fileExists(fileName) {
-		file, err := os.Open(fileName)
-		if err != nil {
-			panic(err.Error())
-		}
-		// Defer file close and panic if there is an error
-		defer func() {
-			if err := file.Close(); err != nil {
-				panic(err.Error())
-			}
-		}()
+func readInput(fileName string, stream *chan []byte, progressStream *chan int64, wg *sync.WaitGroup) {
+	// Defer waitgroup go-routine done before returning
+	defer wg.Done()
 
-		fileInfo, err := file.Stat()
-		if err != nil {
-			panic(err.Error())
-		}
-
-		fileSize := fileInfo.Size()
-
-		*progressStream <- fileSize
-
-		offset := int64(0)
-		for {
-			if fileSize-offset >= sealion.BlockSize {
-				block := make([]byte, sealion.BlockSize)
-				_, err = file.ReadAt(block, offset)
-				if err != nil {
-					panic(err.Error())
-				}
-
-				// PUSH full-block to channel
-				*stream <- block
-				offset += sealion.BlockSize
-				*progressStream <- offset
-			} else if fileSize-offset == 0 { // Once the entire file is read, exit
-				// Send nil to stream to signal end
-				*stream <- nil
-				break
-			} else {
-				block := make([]byte, fileSize-offset)
-				_, err := file.ReadAt(block, offset)
-				if err != nil {
-					panic(err.Error())
-				}
-
-				// PUSH partial-block to channel
-				*stream <- block
-				offset += int64(len(block))
-				*progressStream <- offset
-			}
-		}
-	} else {
-		panic("Input File does not exist")
-	}
-
-	wg.Done()
-}
-
-func progressBar(count int64, progressStream *chan int64) {
-	bar := pb.StartNew(int(count))
-	var prev int
-	var i int
-	for i = 0; i < int(count); {
-		i = int(<-*progressStream)
-		bar.Add(i - prev)
-		prev = i
-	}
-	bar.Finish()
-}
-
-func startWriter(fileName string, stream *chan []byte, wg *sync.WaitGroup) {
-	file, err := os.Create(fileName)
+	// Open input file
+	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// Defer file close and panic if there is an error
 	defer func() {
 		if err := file.Close(); err != nil {
 			panic(err.Error())
 		}
 	}()
 
+	// Get file info
+	fileInfo, err := file.Stat()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Get input file size
+	fileSize := fileInfo.Size()
+
+	// Push input file size as the first input to progress stream
+	*progressStream <- fileSize
+
+	// Initialize offset
 	offset := int64(0)
 	for {
+		if fileSize-offset >= sealion.BlockSize {
+			// Create full-block
+			block := make([]byte, sealion.BlockSize)
+
+			// Read from file at offset to full-block
+			// Panic if there are any errors
+			bytesRead, err := file.ReadAt(block, offset)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			// PUSH full-block to buffered s channel
+			*stream <- block
+			// Increment offset by bytesRead
+			offset += int64(bytesRead)
+
+			// Push offset to buffered progress channel
+			*progressStream <- offset
+		} else if fileSize-offset == 0 {
+			// Exit condition - the entire file is read, exit
+			// Send nil to stream to signal end of input
+			// Break out of loop
+			*stream <- nil
+			break
+		} else {
+			// Create partial block
+			block := make([]byte, fileSize-offset)
+
+			// Read from file at offset to part-block
+			// Panic if there are any errors
+			bytesRead, err := file.ReadAt(block, offset)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			// PUSH partial-block to channel
+			*stream <- block
+			// Increment offset by bytesRead
+			offset += int64(bytesRead)
+
+			// Push offset to buffered progress channel
+			*progressStream <- offset
+		}
+	}
+}
+
+func writeOutput(fileName string, stream *chan []byte, wg *sync.WaitGroup) {
+	// Defer waitgroup go-routine done before returning
+	defer wg.Done()
+
+	// Create output file
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Defer file close and panic if there is an error
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err.Error())
+		}
+	}()
+
+	// Initialize offset
+	offset := int64(0)
+	for {
+		// Read block from stream
 		block := <-*stream
+
+		// A nil block signals end, break out of loop
 		if block == nil {
+			// I WANT TO BREAK FREE
+			// FROM FOR LOOP
 			break
 		}
-		_, err := file.WriteAt(block, offset)
+
+		// Write block to file at offset
+		// Panic if there are any errors
+		bytesWritten, err := file.WriteAt(block, offset)
 		if err != nil {
 			panic(err.Error())
 		}
 
-		offset += int64(len(block))
+		// Increment offset by bytesWritten
+		offset += int64(bytesWritten)
 	}
+}
 
-	wg.Done()
+func progressBar(fileSize int64, progressStream *chan int64) {
+	// Create new progressbar with count
+	bar := pb.StartNew(int(fileSize))
+	for offset := 0; offset < int(fileSize); {
+		offset = int(<-*progressStream)
+		// Set bar to current offset from reader
+		bar.Set(offset)
+	}
+	bar.Finish()
 }
 
 func fileExists(filename string) bool {
@@ -127,7 +158,25 @@ func readFromFile(filePath string) []byte {
 		}
 		return data
 	}
+	// If file doesn't exist, print so and exit
 	fmt.Println("File:", filePath, "seems to be nonexistent")
-	os.Exit(0)
+	os.Exit(1)
+	// Fun fact: if you remove the below return
+	// Compile will fail - as functions which return must
+	// return at the end, BUT... The above OS Exit makes it
+	// such that it'll never execute XD
 	return nil
+}
+
+func printHelp() {
+	// Ah, yes; help.
+	fmt.Printf("%s is a CLI program which implements the SeaLion Block Cipher (http://github.com/sid-sun/sealion) in CFB (cipher feedback) mode with 256-Bit key length, using SHA-256.", os.Args[0])
+	fmt.Printf("\nDeveloped by Sidharth Soni (Sid Sun) <sid@sidsun.com>")
+	fmt.Printf("\nOpen-sourced under The Unlicense")
+	fmt.Printf("\nSource Code: http://github.com/sid-sun/seal-256-cfb\n")
+	fmt.Printf("\nUsage:\n")
+	fmt.Printf("    To encrypt: %s (--encrypt / -e) <input file> <passphrase file> <output file (optional)>\n", os.Args[0])
+	fmt.Printf("    To decrypt: %s (--decrypt / -d) <encrypted input> <passphrase file> <output file (optional)>\n", os.Args[0])
+	fmt.Printf("    To get version number: %s (--version / -v)\n", os.Args[0])
+	fmt.Printf("    To get help: %s (--help / -h)\n", os.Args[0])
 }
